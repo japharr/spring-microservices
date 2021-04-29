553d640083e93115b71bac98ce78208dd5641bd2
Jaleel Adesina <jtutoff@gmail.com>
2021-04-29 17:33:10 +0100

    now using rsocket-loadbalancer

---
+++ driver-service/src/main/java/com/japharr/grpc/driver/config/RSocketConfig.java
@@ -0,0 +1,26 @@
+package com.japharr.grpc.driver.config;
+
+import org.mvnsearch.rsocket.loadbalance.RSocketServiceRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+
+@Configuration
+public class RSocketConfig {
+//    @Bean
+//    public RSocketStrategies rSocketStrategies() {
+//        return RSocketStrategies.builder()
+//            .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
+//            .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+//            .build();
+//    }
+
+//    @Bean
+//    public RSocketRequester carServiceRequester(
+//        RSocketRequester.Builder builder, RSocketServiceRegistry rsocketServiceRegistry) {
+//        return rsocketServiceRegistry.buildLoadBalanceRSocket("CAR-SERVICE", builder);
+//    }
+}
--- driver-service/src/main/java/com/japharr/grpc/driver/service/CarServiceStub.java
+++ driver-service/src/main/java/com/japharr/grpc/driver/service/CarServiceStub.java
@@ -9,6 +9,7 @@ import io.rsocket.loadbalance.RoundRobinLoadbalanceStrategy;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.transport.netty.client.WebsocketClientTransport;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.mvnsearch.rsocket.loadbalance.RSocketServiceRegistry;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -27,33 +28,9 @@ import java.util.List;
 public class CarServiceStub {
     private final Mono<RSocketRequester> rSocketRequester;
 
-    public CarServiceStub(RSocketRequester.Builder rsocketRequesterBuilder, ReactiveDiscoveryClient discoveryClient) {
-        Mono<List<LoadbalanceTarget>> serversMono = discoveryClient
-            .getInstances("CAR-SERVICE")
-            .map(si -> {
-                String tcp = si.getMetadata().getOrDefault("tcp", "5000");
-//                TcpClient tcpClient = TcpClient.create()
-//                    .host(si.getHost()).port(si.getPort());
-                System.out.println("CarServiceStub: url: " + si.getUri().toString());
-                System.out.println("CarServiceStub: path: " + si.getUri().getRawPath());
-                System.out.println("CarServiceStub: port: " + si.getPort());
-                System.out.println("CarServiceStub: tcp-port: " + tcp);
-                HttpClient httpClient = HttpClient.create()
-                    .host(si.getHost()).port(Integer.parseInt(tcp));
-                return LoadbalanceTarget.from(si.getUri().toString(), WebsocketClientTransport.create(httpClient, "/rsocket"));
-                //return LoadbalanceTarget.from(si.getUri().toString(), TcpClientTransport.create(tcpClient));
-            })
-            .collectList();
-
-        this.rSocketRequester = Mono.just(rsocketRequesterBuilder
-            //.setupRoute("/connect")
-            //.setupData("test")
-            .transports(serversMono.flux(), new RoundRobinLoadbalanceStrategy()));
-
-//        this.rSocketRequester = rsocketRequesterBuilder
-//            .rsocketConnector(connector -> connector
-//                .reconnect(Retry.fixedDelay(Integer.MAX_VALUE, Duration.ofSeconds(1))))
-//            .connectTcp("localhost", 7001).cache();
+    public CarServiceStub(RSocketRequester.Builder rsocketRequesterBuilder, ReactiveDiscoveryClient discoveryClient, RSocketServiceRegistry rsocketServiceRegistry) {
+        this.rSocketRequester = Mono.just(rsocketServiceRegistry
+            .buildLoadBalanceRSocket("car-service", rsocketRequesterBuilder));
     }
 
     public Mono<CarDto> getCarById (Long id) {
